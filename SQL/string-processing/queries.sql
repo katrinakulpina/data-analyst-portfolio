25cead0888fc1c6ee65c7181f2182ae72c53a546
Ekaterina Kulpina <125750430+katrinakulpina@users.noreply.github.com>
2023-02-19 13:21:19 +0300

    Update queries.sql

--- SQL/string-processing/queries.sql
+++ SQL/string-processing/queries.sql
@@ -1,92 +1,82 @@
-create table demipt3.klpn_log (
-	dt date,
-	link varchar(50),
-	user_agent varchar(200),
-	region varchar(30)
+-- Задание 1
+CREATE TABLE log (
+	dt DATE,
+	link VARCHAR(50),
+	user_agent VARCHAR(200),
+	region VARCHAR(30)
 );
 
-insert into demipt3.klpn_log (dt, link, user_agent, region)
-select 
-	cast(dt as date),
-	cast(link as varchar(50)),
-	cast(user_agent as varchar(200)),
-	cast(region as varchar(30))
-from (
-	select ip, dt, link, n1,
-			trim(substr(data, 1, strpos(data, ' '))) as n2,
-			trim(substr(data, strpos(data, ' '))) as user_agent
-		from (
-		select 
-			ip,
-			dt,
-			link,
-			trim(substr(data, 1, strpos(data, ' '))) as n1,
-			trim(substr(data, strpos(data, ' '))) as data
-		from (
-			select 
+INSERT INTO log (dt, link, user_agent, region)
+SELECT 
+	CAST(dt AS DATE),
+	CAST(link AS VARCHAR(50)),
+	CAST(user_agent AS VARCHAR(200)),
+	CAST(region AS VARCHAR(30))
+FROM (
+	SELECT ip, dt, link, n1,
+		TRIM(SUBSTR(data, 1, STRPOS(data, ' '))) AS n2,
+		TRIM(SUBSTR(data, STRPOS(data, ' '))) AS user_agent
+	FROM (
+		SELECT ip, dt, link,
+			TRIM(SUBSTR(data, 1, STRPOS(data, ' '))) AS n1,
+			TRIM(SUBSTR(data, STRPOS(data, ' '))) AS data
+		FROM (
+			SELECT 
 				ip,
 				dt,
-				trim(substr(data, 1, strpos(data, ' '))) as link,
-				trim(substr(data, strpos(data, ' '))) as data
-			from (
-				select 
-					trim(substr(data, 1, strpos(data, ' '))) as ip,
-					to_timestamp(trim(substr(data, strpos(data, ' ')+1, 14)), 'YYYYMMDDHH24MISS') as dt,
-					trim(substr(data, strpos(data, ' ')+15)) as data
-				from (
-					select 
-						regexp_replace(data, '\t+', ' ', 'g') as data
-					from de.log
-					where data is not null
+				TRIM(SUBSTR(data, 1, STRPOS(data, ' '))) AS link,
+				TRIM(SUBSTR(data, STRPOS(data, ' '))) AS data
+			FROM (
+				SELECT 
+					TRIM(SUBSTR(data, 1, STRPOS(data, ' '))) AS ip,
+					TO_TIMESTAMP(TRIM(SUBSTR(data, STRPOS(data, ' ')+1, 14)), 'YYYYMMDDHH24MISS') AS dt,
+					TRIM(SUBSTR(data, STRPOS(data, ' ')+15)) AS data
+				FROM (
+					SELECT 
+						REGEXP_REPLACE(data, '\t+', ' ', 'g') AS data
+					FROM de_log
+					WHERE data IS NOT NULL
 				) t1
 			) t2
 		) t3
 	) t4
-) log left join (
-	select
-		trim(substr(data, 1, strpos(data, ' '))) as ip,
-		trim(substr(data, strpos(data, ' '))) as region
-	from (
-		select
-			regexp_replace(data, '\t', ' ') as data 
-		from de.ip
+) l LEFT JOIN (
+	SELECT
+		TRIM(SUBSTR(data, 1, STRPOS(data, ' '))) AS ip,
+		TRIM(SUBSTR(data, STRPOS(data, ' '))) AS region
+	FROM (
+		SELECT
+			REGEXP_REPLACE(data, '\t', ' ') AS data 
+		FROM de_ip
 	) t
 ) ip 
-on log.ip = ip.ip;
+ON l.ip = ip.ip;
 
 
-create table demipt3.klpn_log_report (
-	region varchar(30),
-	browser varchar(10)
+-- Задание 2
+CREATE TABLE log_report (
+	region VARCHAR(30),
+	browser VARCHAR(10)
 );
 
-insert into demipt3.klpn_log_report (region, browser)
-select 
+INSERT INTO log_report (region, browser)
+SELECT 
 	region,
-	cast(browser as varchar(10))
-from (
-	with rep as (
-		select 
+	CAST(browser AS VARCHAR(10))
+FROM (
+	WITH rep AS (
+		SELECT 
 			region,
-			trim(substr(user_agent, 1, strpos(user_agent, '/')-1)) as browser,
-			count(*) as cnt
-		from demipt3.klpn_log
-		group by region, trim(substr(user_agent, 1, strpos(user_agent, '/')-1))
+			TRIM(SUBSTR(user_agent, 1, STRPOS(user_agent, '/')-1)) AS browser,
+			COUNT(*) AS cnt
+		FROM log
+		GROUP BY region, TRIM(SUBSTR(user_agent, 1, STRPOS(user_agent, '/')-1))
 	)
-	select 
+	SELECT 
 		region,
-		min(browser) as browser
-	from rep r
-	where cnt = (select max(cnt) from rep r1 where r1.region = r.region)
-	group by region
+		MIN(browser) AS browser
+	FROM rep r
+	WHERE cnt = (SELECT MAX(cnt) FROM rep r1 WHERE r1.region = r.region)
+	GROUP BY region
 ) report;
-
-
-
-
-
-
-
-
-
 
